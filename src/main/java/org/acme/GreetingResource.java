bbc1c0acd841c3aeef84cb521bb82151febb1836
Tom Schneider <ric0.github@gmail.com>
2022-09-28 01:17:38 +0200

    feat: add GreetingService and Endpoint with dynamic path variable

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -1,5 +1,6 @@
 package org.acme;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -7,6 +8,17 @@ import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService greetingService;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}") // curl localhost:8080/hello/greeting/ric0
+    public String greeting(String name) {
+        System.out.println("called endpoints");
+        return greetingService.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
